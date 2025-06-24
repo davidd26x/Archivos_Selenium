d513ec830714c88a3a9771c190b699207c711e8e
Jose <jose01265@gmail.com>
2025-06-24 07:21:28 -0600

    Agregacion de BaseTest al proyecto SeleniumTestNG

---
+++ SeleniumTestNG/src/test/java/utilities/BaseTest.java
@@ -0,0 +1,42 @@
+package utilities;
+
+import listeners.SuiteListeners;
+import listeners.TestListeners;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.asserts.SoftAssert;
+
+@Listeners({TestListeners.class, SuiteListeners.class})
+public class BaseTest {
+    protected SoftAssert softAssert;
+    protected WebDriver driver;
+    protected final String regression = "regression";
+    protected final String smoke = "smoke";
+
+    @BeforeMethod(alwaysRun = true)
+    public void masterSetUp() {
+        softAssert = new SoftAssert();
+
+        Logs.debug("Inicializando el driver");
+        driver = new ChromeDriver();
+
+        Logs.debug("Maximizando la pantalla");
+        driver.manage().window().maximize();
+
+        Logs.debug("Borrando cookies");
+        driver.manage().deleteAllCookies();
+
+        Logs.debug("Asignando driver al webdriver provider");
+        new WebdriverProvider().set(driver);
+        
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void masterTearDown() {
+        Logs.debug("Matando el driver");
+        driver.quit();
+    }
+}
